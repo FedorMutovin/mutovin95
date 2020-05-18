2f5876641d435694748d9c15dbead221e89decb1
Anakin Skywalker <mutovin9@mail.ru>
2020-05-18 13:01:58 -0400

    "Урок 1.Площадь треугольника"

---
+++ Lesson 1/Площадь треугольника.rb
@@ -0,0 +1,6 @@
+puts "Введите основание треугольника"
+a = gets.to_i
+puts "Введите высоту треугольника"
+h = gets.to_i
+triangle_area = (1.0/2)*a*h
+puts "Площадь треугольника равна #{triangle_area}"
