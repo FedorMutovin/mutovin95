ad3bc5164868ff79d33829f1a8fec8e2d075e657
Anakin Skywalker <mutovin9@mail.ru>
2020-05-18 12:43:25 -0400

    "Урок 1.Идеальный вес"

---
+++ Lesson 1/Идеальный вес.rb
@@ -0,0 +1,10 @@
+puts "Введите ваше имя?"
+name = gets.chomp
+puts "Введите ваш рост?"
+height = gets.to_i
+perfect_weight = ((height - 110) * 1.15)
+if perfect_weight < 0
+  puts "#{name}, твой вес оптимален"
+else
+  puts "#{name}, твой идеальный вес #{perfect_weight} кг."
+end
