09f082ef969664c07f296351a26c989305fc0c69
Anakin Skywalker <mutovin9@mail.ru>
2020-05-19 05:02:25 -0400

    "Урок 1.Доработка квадратного уравнения"

--- Lesson 1/quadratic_equation.rb
+++ Lesson 1/quadratic_equation.rb
@@ -9,7 +9,6 @@ c = gets.to_f
 
 discriminant = b**2 - 4 * a * c
 minor_b = ~b + 1
-discriminant_square_root = Math.sqrt(discriminant)
 
 if discriminant.negative?
   puts 'Корней нет'
@@ -17,6 +16,7 @@ elsif discriminant.zero?
   x = minor_b / (2 * a)
   puts "Дискриминант равен #{discriminant}, 1 корень #{x}"
 else
+  discriminant_square_root = Math.sqrt(discriminant)
   first_x = (minor_b + discriminant_square_root) / (2 * a)
   second_x = (minor_b - discriminant_square_root) / (2 * a)
   puts "Два корня, Х1 = #{first_x}, Х2 = #{second_x}"
